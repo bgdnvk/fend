7745797eb5211331b418ae34bce2692b70ca0398
bgdnvk <bnovykov@gmail.com>
2020-05-06 18:44:07 +0200

    added nav

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,8 +17,15 @@
  * Define Global Variables
  * 
 */
+//get header and navbar
+const pageHeader = document.querySelector(".page__header");
+const navBarMenu = document.querySelector(".navbar__menu");
 
-
+const navBarList = document.getElementById("navbar__list");
+//get sections of the page
+const sections = document.querySelectorAll("section");
+//get the h2 of the section
+const header2 = document.querySelector("h2.landing__container");
 /**
  * End Global Variables
  * Start Helper Functions
@@ -34,7 +41,30 @@
 */
 
 // build the nav
+function buildNav(){
+    //making a fragment for better performance as per lesson 5
+    const fragment = document.createDocumentFragment();
 
+    for(let i = 0; i < sections.length; i++){
+        const newLi = document.createElement("li");
+        newLi.innerHTML =`<a href="#${sections[i].id}">${
+            //selecting the inner text (inner html) of the header
+            sections[i].querySelector("h2").innerText}
+            </a>`;
+        //adding some style
+        newLi.style.margin = "1%";
+        newLi.style.padding = "1%";
+        newLi.style.border = "1px solid purple";
+        
+
+        fragment.appendChild(newLi);
+    }
+    //checking if it works properly
+    //document.body.appendChild(fragment);
+    navBarList.appendChild(fragment);
+}
+//executing the function
+buildNav();
 
 // Add class 'active' to section when near top of viewport
 
@@ -53,5 +83,3 @@
 // Scroll to section on link click
 
 // Set sections as active
-
-
